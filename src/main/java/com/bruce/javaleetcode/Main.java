e0fd0e6c700f05bd61f73a70e210b3d5f5aad1e1
Bu,Shaopeng <bushaopeng@baidu.com>
2016-10-31 14:16:38 +0800

    first init

---
+++ src/main/java/com/bruce/javaleetcode/Main.java
@@ -0,0 +1,10 @@
+package com.bruce.javaleetcode;
+
+/**
+ * Created by bushaopeng on 16/10/31.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("this is my proj");
+    }
+}
